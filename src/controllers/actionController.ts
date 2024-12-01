4b86254caac67d8a46d77ea3671d293798d5d7b0
Andrey Ligiza <andrey.ligiza@mail.ru>
2024-12-01 16:47:58 +0300

    refactor: разделение обновления и создания действий

--- src/controllers/actionController.ts
+++ src/controllers/actionController.ts
@@ -1,38 +1,71 @@
-import { User } from "../db";
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../middlewares/error";
 import { checkReqFields } from "../utils/checkReqFields";
 import { Action } from "../db/models/Action";
-import { Json } from "sequelize/types/utils";
 
 const ActionType = ["green", "yellow", "blue", "gray"] as const;
+
 type ActionType = (typeof ActionType)[number];
+
 const isActionType = (value: unknown): value is ActionType => {
     return ActionType.includes(value as ActionType);
 };
 
 type CreateActionRequest = {
-    actionId: number;
     userId: number;
     title: string;
     type: ActionType;
-    cost: number;
 };
 
+type UpdateActionsRequest = {
+    actionId: number;
+    cost: number;
+} & CreateActionRequest;
+
 class ActionController {
-    createOrUpdate = async (
-        req: Request,
+    create = async (
+        req: Request<{}, {}, CreateActionRequest[]>,
         res: Response,
         next: NextFunction
     ) => {
         try {
-            const body = req.body as CreateActionRequest[];
+            const body = req.body;
 
             if (Array.isArray(body) === false) {
                 throw new ApiError(400, "Request body must be array");
             }
             body.forEach((action) => {
                 checkReqFields(next, [
+                    action.userId,
+                    action.title,
+                    action.type,
+                ]);
+
+                if (isActionType(action.type) === false) {
+                    throw new ApiError(400, "Invalid type value");
+                }
+            });
+            const action = await Action.bulkCreate(body);
+            res.json(action);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    updateActions = async (
+        req: Request<{}, {}, UpdateActionsRequest[]>,
+        res: Response,
+        next: NextFunction
+    ) => {
+        try {
+            const body = req.body;
+
+            if (Array.isArray(body) === false) {
+                throw new ApiError(400, "Request body must be array");
+            }
+            body.forEach((action) => {
+                checkReqFields(next, [
+                    action.actionId,
                     action.userId,
                     action.title,
                     action.type,
@@ -62,6 +95,7 @@ class ActionController {
 
             const actions = await Action.findAll({
                 where: { userId, type },
+                order: [["actionId", "ASC"]],
             });
             res.json(actions);
         } catch (error) {
@@ -74,6 +108,8 @@ class ActionController {
             const { userId } = req.query as { userId: string };
             const actions = await Action.findAll({
                 where: { userId },
+                order: [["actionId", "ASC"]],
+
             });
             res.json(actions);
         } catch (error) {
--- src/routes/action.ts
+++ src/routes/action.ts
@@ -3,7 +3,8 @@ import { actionController } from "../controllers/actionController";
 
 export const actionRouter = Router();
 
-actionRouter.post("/", actionController.createOrUpdate);
+actionRouter.post("/", actionController.create);
+actionRouter.patch("/", actionController.updateActions);
 actionRouter.get("/by-type", actionController.getByType); // ?userId= & type=
 actionRouter.get("/by-user", actionController.getByUser); // ?userId=
 actionRouter.delete("/:id", actionController.delete); 
